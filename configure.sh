4e04ce8d40d3afbb57fecb02b83e8fbe0fd388a4
Yuki Kikuchi <bclswl0827@foxmail.com>
2019-10-06 23:00:10 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@
 cat /dev/null > /etc/v2ray/config.json
 # V2Ray new configuration
 #echo "${CONFIG_JSON}" > /etc/v2ray/config.json
-echo '{"inbounds":[{"port":80,"protocol":"vmess","settings":{"clients":[{"id":"ad806487-2d26-4636-98b6-ab85cc8521f7","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ws"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > /etc/v2ray/config.json
+echo '{"inbounds":[{"port":${PORT},"protocol":"vmess","settings":{"clients":[{"id":"ad806487-2d26-4636-98b6-ab85cc8521f7","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ws"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > /etc/v2ray/config.json
