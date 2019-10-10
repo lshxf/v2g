2dabb89b9712dc7bac76637ebb8468538e4aaac9
Yuki Kikuchi <bclswl0827@foxmail.com>
2019-10-10 19:09:01 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
+# Install V2Ray
+curl https://install.direct/go.sh | bash
+# Remove extra functions
+rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # Clear previous configuration
 cat /dev/null > /etc/v2ray/config.json
 # V2Ray new configuration
-echo "$CONFIG_JSON" > /etc/v2ray/config.json
+cat <<-EOF > /etc/v2ray/config.json
+{
+	"inbounds": [{
+		"port": 80,
+		"protocol": "vmess",
+		"settings": {
+			"clients": [{
+				"id": "${UUID}",
+				"alterId": 64
+			}]
+		},
+		"streamSettings": {
+			"network": "ws"
+		}
+	}],
+	"outbounds": [{
+		"protocol": "freedom",
+		"settings": {}
+	}]
+}
+EOF
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
