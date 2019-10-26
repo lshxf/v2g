d439d07c2b699238863c6dfdd8be54abc597fa23
Yuki Kikuchi <bclswl0827@foxmail.com>
2019-10-26 12:50:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,8 +3,6 @@
 curl https://install.direct/go.sh | bash
 # Remove extra functions
 rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
-# Clear previous configuration
-cat /dev/null > /etc/v2ray/config.json
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
 {
