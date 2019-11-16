d8c5294cbdb5ad6707fceee2cccacee953ed3211
Yuki Kikuchi <bclswl0827@foxmail.com>
2019-11-16 20:36:20 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install V2Ray
-bash <(wget --no-check-certificate -qO- 'https://install.direct/go.sh')
+curl https://install.direct/go.sh | bash
 # Remove extra functions
 rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # V2Ray new configuration
