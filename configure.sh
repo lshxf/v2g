ab0505188045c8f294841771a877a733121e81b1
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-02-20 18:53:57 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,10 +3,10 @@
 curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 mkdir /usr/bin/v2ray /etc/v2ray
 touch /etc/v2ray/config.json
-unzip /v2ray.zip "v2ray" -d /usr/bin/v2ray
-unzip /v2ray.zip "v2ctl" -d /usr/bin/v2ray
-# Remove /v2ray.zip
-rm -rf /v2ray.zip
+unzip /v2ray.zip -d /usr/bin/v2ray
+# Remove /v2ray.zip and other useless functions
+rm `ls /usr/bin/v2ray | grep -v "v2"`
+rm -rf /v2ray.zip /usr/bin/v2ray/v2ray.sig /usr/bin/v2ray/v2ctl.sig 
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
 {
