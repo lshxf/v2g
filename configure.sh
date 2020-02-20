803b233ff081a2b3dce9a1df07d11740c629f701
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-02-20 18:38:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 # Download and install V2Ray
 curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 mkdir /usr/bin/v2ray /etc/v2ray
+touch /etc/v2ray/config.json
 unzip /v2ray.zip "v2ray" -d /usr/bin/v2ray
 unzip /v2ray.zip "v2ctl" -d /usr/bin/v2ctl
 # Remove /v2ray.zip
