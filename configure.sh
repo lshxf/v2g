0f74e2ba1ff976793231b2c085b8dcede3eba7cd
lshxf <66930825+lshxf@users.noreply.github.com>
2022-10-21 14:07:08 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 64
+                        "alterId": 0
                     }
                 ],
                 "disableInsecureEncryption": true
